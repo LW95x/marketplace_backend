1bb18ab135262bd656e9af0a7b1929af873a48e1
Liam Woodall <liam.woodall@live.co.uk>
2025-05-20 23:49:54 +0100

    further amendments to signalR

--- Marketplace/Helpers/MessageHub.cs
+++ Marketplace/Helpers/MessageHub.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Marketplace.BusinessLayer;
+using Marketplace.DataAccess.Repositories;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Marketplace.Helpers
 {
     public class MessageHub : Hub
     {
+        private readonly IMessageRepository _messageRepository;
+
+        public MessageHub(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository ?? throw new ArgumentNullException(nameof(messageRepository));
+        }
         public async Task SendMessage(string senderId, string receiverId, string content)
         {
+            await _messageRepository.SaveMessage(senderId, receiverId, content);
+
             await Clients.User(receiverId).SendAsync("ReceiveMessage", senderId, content);
         }
     }
