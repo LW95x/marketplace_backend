f8a112000eb0d5d6385c6778121e6b1f5ed0962d
Liam Woodall <liam.woodall@live.co.uk>
2025-06-15 23:36:32 +0100

    using gmail server

--- Marketplace/Helpers/EmailService.cs
+++ Marketplace/Helpers/EmailService.cs
@@ -26,7 +26,7 @@ namespace Marketplace.Helpers
             message.Body = body.ToMessageBody();
 
             using var client = new SmtpClient();
-            await client.ConnectAsync(_config["EmailSettings:SmtpServer"], int.Parse(_config["EmailSettings:SmtpPort"] ?? "587"), MailKit.Security.SecureSocketOptions.StartTls);
+            await client.ConnectAsync("smtp.gmail.com", int.Parse(_config["EmailSettings:SmtpPort"] ?? "587"), MailKit.Security.SecureSocketOptions.StartTls);
             await client.AuthenticateAsync(_config["EmailSettings:SmtpUsername"], _config["EmailSettings:SmtpPassword"]);
 
             await client.SendAsync(message);
